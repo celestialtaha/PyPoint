af61eb67ba198a8494e2af56aeeb59cb8dbc6bcb
Taha Samavati <tahasamavati11@yahoo.com>
2022-02-14 10:48:57 +0330

    update code

--- pypoint.py
+++ pypoint.py
@@ -5,7 +5,10 @@ import config
 """TODO
         1. Add sys args
         2. Unique color for each class
-        3. Write class name on the image when labeling 
+        3. Add MS COCO Saving format
+        4. Add save as json
+        5. Add b.b labeling feature
+        6. Add directory mode and single mode labeling (sys-arg)
 """
 
 classes = ['sedan', 'bus', 'van', 'pickup']
@@ -16,6 +19,13 @@ def set_curr_class_idx(reset=-1):
         config.curr_class_idx = 0
     else:
         config.curr_class_idx +=1
+
+def update_stat():
+    img_stat[img.shape[0]:, :,:] = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img_stat,f"Labeling {classes[config.curr_class_idx]}", (10, img.shape[0]+30), font, 1, (255,255,255),2)
+    cv2.putText(img_stat,f"Guide: q: save&exit / u: undo / leftMouse: Place label / rightMouse: Next class", (10, img.shape[0]+60), font, 0.5, (255,255,255),1)
+    print(f"Switched to the next class...\nPlease check the points corresponding to the {classes[config.curr_class_idx]} class")
 
 # function to display the coordinates of
 # of the points clicked on the image
@@ -31,15 +41,11 @@ def click_event(event, x, y, flags, params):
             labels[classes[config.curr_class_idx]].append((x,y))
         except:
             labels[classes[config.curr_class_idx]] = [(x,y)]
-        # displaying the coordinates
-        # on the image window
-        #font = cv2.FONT_HERSHEY_SIMPLEX
-        image = cv2.circle(img, (x,y), radius=3, color=(0, 0, 255), thickness=-1)
-        #cv2.putText(img, str(x) + ',' +
-        #            str(y), (x,y), font,
-        #            1, (255, 0, 0), 2)
+        
+        # displaying the dot
+        image = cv2.circle(img_stat, (x,y), radius=3, color=(0, 0, 255), thickness=-1)
 
-        cv2.imshow('image', img)
+        cv2.imshow('image', img_stat)
  
     # checking for right mouse clicks    
     elif event==cv2.EVENT_RBUTTONDOWN:
@@ -49,18 +55,21 @@ def click_event(event, x, y, flags, params):
         set_curr_class_idx()
         if config.curr_class_idx> (len(classes) - 1):
             set_curr_class_idx(1)
-        print(f"Switched to the next class...\nPlease check the points corresponding to the {classes[config.curr_class_idx]} class")
-        cv2.imshow('image', img)
+        
+        update_stat()
+        cv2.imshow('image', img_stat)
     else:
         pass
  
 if __name__=="__main__": 
     # reading the image
     img = cv2.imread('./images/cars.jpg', 1)
+    img_stat = np.zeros((img.shape[0]+100, img.shape[1], img.shape[2]),dtype=img.dtype)
+    img_stat[:img.shape[0],:img.shape[1],:] = img
  
     # displaying the image
-    cv2.imshow('image', img)
-    print(f"Please check the points corresponding to the {classes[config.curr_class_idx]} class")
+    update_stat()
+    cv2.imshow('image', img_stat)    
  
     # setting mouse handler for the image
     # and calling the click_event() function
@@ -74,3 +83,4 @@ if __name__=="__main__":
 
     with open('./cars.pkl', 'wb') as f:
         pickle.dump(labels, f)
+        print("The label was succesfully saved!")
